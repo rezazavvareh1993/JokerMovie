4f44dc90966f412477a869f971464b8ad783a551
r-zavareh <r-zavareh@agri-bank.com>
2025-10-08 14:23:22 +0330

    remove JUnit

--- build-logic/convention/src/main/java/com/rezazavareh7/convention/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/rezazavareh7/convention/AndroidApplicationConventionPlugin.kt
@@ -65,11 +65,9 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                 add("implementation", libs.findLibrary("androidx-core-ktx").get())
                 add("implementation", libs.findLibrary("androidx-lifecycle-runtime-ktx").get())
                 add("implementation", libs.findLibrary("androidx-core-splashscreen").get())
-
                 add("testImplementation", libs.findLibrary("junit").get())
                 add("androidTestImplementation", libs.findLibrary("androidx-junit").get())
                 add("androidTestImplementation", libs.findLibrary("androidx-espresso-core").get())
-                add("androidTestImplementation", libs.findLibrary("androidx-ui-test-junit4").get())
                 add("debugImplementation", libs.findLibrary("kotlinx-coroutines-test").get())
             }
         }
