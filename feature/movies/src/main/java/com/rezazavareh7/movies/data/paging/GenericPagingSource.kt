2e6dfb515525302b438f3464dfe552a6c66afa62
r-zavareh <r-zavareh@agri-bank.com>
2025-10-19 16:33:23 +0330

    Remove additional paging source

--- feature/movies/src/main/java/com/rezazavareh7/movies/data/paging/GenericPagingSource.kt
+++ feature/movies/src/main/java/com/rezazavareh7/movies/data/paging/GenericPagingSource.kt
@@ -3,38 +3,35 @@ package com.rezazavareh7.movies.data.paging
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.rezazavareh7.movies.domain.networkstate.BasicNetworkState
-import javax.inject.Inject
 
-class GenericPagingSource<ResponseType, OutputType : Any>
-    @Inject
-    constructor(
-        private val apiCall: suspend (page: Int) -> ResponseType,
-        private val mapper: suspend (ResponseType) -> BasicNetworkState<List<OutputType>>,
-    ) : PagingSource<Int, OutputType>() {
-        override fun getRefreshKey(state: PagingState<Int, OutputType>): Int? =
-            state.anchorPosition?.let { anchorPosition ->
-                state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
-                    ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+class GenericPagingSource<ResponseType, OutputType : Any> constructor(
+    private val apiCall: suspend (page: Int) -> ResponseType,
+    private val mapper: suspend (ResponseType) -> BasicNetworkState<List<OutputType>>,
+) : PagingSource<Int, OutputType>() {
+    override fun getRefreshKey(state: PagingState<Int, OutputType>): Int? =
+        state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, OutputType> =
+        try {
+            val page = params.key ?: 1
+            val response = apiCall(page)
+            val result = mapper(response)
+
+            when (result) {
+                is BasicNetworkState.Success ->
+                    LoadResult.Page(
+                        data = result.data,
+                        prevKey = if (page == 1) null else page - 1,
+                        nextKey = if (result.data.isEmpty()) null else page + 1,
+                    )
+
+                is BasicNetworkState.Error ->
+                    LoadResult.Error(result.throwable)
             }
-
-        override suspend fun load(params: LoadParams<Int>): LoadResult<Int, OutputType> =
-            try {
-                val page = params.key ?: 1
-                val response = apiCall(page)
-                val result = mapper(response)
-
-                when (result) {
-                    is BasicNetworkState.Success ->
-                        LoadResult.Page(
-                            data = result.data,
-                            prevKey = if (page == 1) null else page - 1,
-                            nextKey = if (result.data.isEmpty()) null else page + 1,
-                        )
-
-                    is BasicNetworkState.Error ->
-                        LoadResult.Error(result.throwable)
-                }
-            } catch (e: Exception) {
-                LoadResult.Error(e)
-            }
-    }
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+}
