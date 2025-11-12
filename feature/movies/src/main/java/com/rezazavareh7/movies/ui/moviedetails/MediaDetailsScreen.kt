a96da91dad8f58b1d5e69507608b3b2fe5d5578a
r-zavareh <r-zavareh@agri-bank.com>
2025-11-12 08:52:21 +0330

    Add animation to visible credits

--- feature/movies/src/main/java/com/rezazavareh7/movies/ui/moviedetails/MediaDetailsScreen.kt
+++ feature/movies/src/main/java/com/rezazavareh7/movies/ui/moviedetails/MediaDetailsScreen.kt
@@ -1,6 +1,7 @@
 package com.rezazavareh7.movies.ui.moviedetails
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
@@ -262,7 +263,7 @@ fun MediaDetailsScreen(
                                         text = mediaDetailsUiState.movieDetailsData.overview,
                                     )
                                 }
-                                if (mediaDetailsUiState.mediaCredits.isNotEmpty()) {
+                                AnimatedVisibility(visible = mediaDetailsUiState.mediaCredits.isNotEmpty()) {
                                     Spacer(Modifier.height(8.dp))
                                     TitleMediumTextComponent(text = stringResource(R.string.credits))
                                     LazyRow(
